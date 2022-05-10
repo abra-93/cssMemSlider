390ff738baee619415f6ab395372118148f8e45c
Alex Br <maskall1993@gmail.com>
2022-05-10 17:32:49 +0300

    feat: add slider functionality (Tue May 10 2022 17:32:31 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,10 +1,12 @@
-const ACTIVE_IMG = document.querySelectorAll(".slider__image");
+const IMG = document.querySelectorAll(".slider__image");
+const ACTIVE_IMG = document.querySelector(".active-image");
 const NEXT_IMG = document.querySelector(".next-image");
 const SLIDER_CONTROL = document.querySelectorAll(".list__item");
 const LIST_DOT = document.querySelectorAll(".list__dot");
 const TEXT_ITEM = document.querySelectorAll(".text__item");
 
 const changeText = (id) => {
+  NEXT_IMG.children[0].src = `./assets/gif/${+id}.gif`;
   TEXT_ITEM.forEach((text) => {
     text.classList.remove("active-text");
     if (+text.dataset.id == id) {
@@ -18,6 +20,20 @@ const changeText = (id) => {
   });
 };
 
+const changeImage = (id) => {
+  let nextImg = ACTIVE_IMG.nextElementSibling || IMG[0];
+  ACTIVE_IMG.classList.add("hide");
+  nextImg.classList.add("show");
+  console.log(id);
+  setTimeout(() => {
+    ACTIVE_IMG.classList.remove("hide");
+    nextImg.classList.remove("show");
+    ACTIVE_IMG.children[0].src = `./assets/gif/${id}.gif`;
+  }, 500);
+
+  console.log(nextImg);
+};
+
 const clickDot = (dot) => {
   let id = dot.dataset.id;
 
@@ -26,6 +42,7 @@ const clickDot = (dot) => {
   });
   dot.children[0].classList.add("active-dot");
   changeText(id);
+  changeImage(id);
 };
 
 SLIDER_CONTROL.forEach((dot) => {
